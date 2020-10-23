7e774ff11bff7d12dfa797b9e55d21b16b1b5281
Rafael <rafaelmsilva2007@hotmail.com>
2020-10-23 10:42:38 -0300

    Admin - User - adjust

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -3,6 +3,7 @@
 namespace Hcode;
 
 use Rain\Tpl;
+use \Hcode\Model\User;
 
 class Page {
 
@@ -26,6 +27,12 @@ class Page {
 		Tpl::configure( $config );
 
 		$this->tpl = new Tpl;
+
+		if (isset($_SESSION[User::SESSION])){
+
+			$this->tpl->assign("user", $_SESSION[User::SESSION]);
+			
+		} 
 
 		$this->setData($this->options["data"]);
 
